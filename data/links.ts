4e0cc7d29cf8fab9cae11f9ca793e07c41cb051a
Kota Ito <boooocchi@gmail.com>
2023-08-04 17:15:08 -0700

    setup: data base with prisma

--- data/links.ts
+++ data/links.ts
@@ -2,34 +2,34 @@ export const links = [
   {
     category: "Open Source",
     description: "Fullstack React framework",
-    id: 1,
+    id: "1",
     imageUrl: "https://github.com/vercel.png",
     title: "Next.js",
-    url: "https://nextjs.org",
+    url: "https://nextjs.org"
   },
   {
     category: "Open Source",
     description: "Next Generation ORM for TypeScript and JavaScript",
-    id: 2,
+    id: "2",
     imageUrl: "https://github.com/prisma.png",
 
     title: "Prisma",
-    url: "https://prisma.io",
+    url: "https://prisma.io"
   },
   {
     category: "Open Source",
     description: "Utility-fist css framework",
-    id: 3,
+    id: "3",
     imageUrl: "https://github.com/tailwindlabs.png",
     title: "TailwindCSS",
-    url: "https://tailwindcss.com",
+    url: "https://tailwindcss.com"
   },
   {
     category: "Open Source",
     description: "GraphQL implementation ",
-    id: 4,
+    id: "4",
     imageUrl: "https://www.apollographql.com/apollo-home.jpg",
     title: "Apollo GraphQL",
-    url: "https://apollographql.com",
-  },
+    url: "https://apollographql.com"
+  }
 ];
