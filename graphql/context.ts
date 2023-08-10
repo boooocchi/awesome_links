c302b4abee9380feede676dd856a2f27c968e887
Kota Ito <boooocchi@gmail.com>
2023-08-10 14:08:48 -0700

    delete the builder file and fixed the bug about login

--- graphql/context.ts
+++ graphql/context.ts
@@ -10,12 +10,13 @@ export type Context = {
 };
 
 export async function createContext({ req, res }): Promise<Context> {
-  const session = getSession(req, res);
+  const session = await getSession(req, res);
 
-  // if the user is not logged in, omit returning the user and accessToken
   if (!session) return { prisma };
 
   const { user, accessToken } = session;
+  console.log(session);
+  console.log(user);
 
   return {
     user,
